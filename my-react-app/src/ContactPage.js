f8b1649260a534e0b4b2917555f1730b8ca2c08e
celiahouston <celia.houston99@gmail.com>
2024-01-30 16:48:23 +0000

    added styling

--- my-react-app/src/ContactPage.js
+++ my-react-app/src/ContactPage.js
@@ -1,11 +1,13 @@
 import React from "react";
+import headshotImage from "./assets/images/headshot.png";
 
 function ContactPage() {
     return (
-        <div> 
-            <h1> Contact Me </h1>
-            <p>Feel free to get in touch with me if you have any questions.</p>
-            <ul>
+        <div className="contact-page"> 
+            <h1 className="title"> Contact Me </h1>
+            <img src={require("./assets/images/headshot.png")} alt="Celia Houston Headshot" className="img-resize" />
+            <p className="subtitle">Feel free to get in touch with me if you have any questions!</p>
+            <ul className="description">
                 <li> Email: celia.houston99@gmail.com</li>
                 <li>LinkedIn: <a href="https://www.linkedin.com/in/celia-houston-69a210155" target="_blank" rel="noopener noreferrer">www.linkedin.com/in/celia-houston-69a210155</a></li>
                 <li> GitHub: <a href="https://github.com/celiahouston" target="_blank" rel="noopener noreferrer">https://github.com/celiahouston</a></li>
--- my-react-app/src/HomePage.js
+++ my-react-app/src/HomePage.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function HomePage() {
     return (
-        <div> 
-            <h1> Welcome to My Portfolio!</h1>
-            <p>Hello, I'm Celia Houston, a web developer.</p>
-            <p> This is my portfolio website where I showcase the projects I have completed.</p>
+        <div className="home-page"> 
+            <h1 className="title"> Welcome to My Portfolio!</h1>
+            <p className="subtitle">Hello, I'm Celia Houston, a web developer.</p>
+            <p className="description"> This is my portfolio website where I showcase the projects I have completed.</p>
         </div>
     )
 }
